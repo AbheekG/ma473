1050370d67a448e2369b29de7580935591ca94f0
Abheek Ghosh <abheekghosh.ag@gmail.com>
2018-01-12 13:11:14 +0530

    ... lab 1

--- abheekMA473lab1/q2.m
+++ abheekMA473lab1/q2.m
@@ -87,3 +87,49 @@ function [U] = BTCS(h, k, m, n, fun, f, g1, g2)
 
 	U
 end
+
+function [U] = CTCS(h, k, m, n, fun, f, g1, g2)
+	fprintf('\nRunning CTCS\n');
+	lamda = k / h^2;
+	U = zeros(n+1, m+1);
+
+	A = zeros((m+1)*(n+1), (m+1)*(n+1));
+	b = zeros((m+1)*(n+1), 1);
+	U(1, 1:end) = f((0:m)*h);
+	U(1:end, 1) = g1((0:n)*k);
+	U(1:end, end) = g2((0:n)*k);
+
+	for i = 2:n
+		for j = 2:m
+			% (i-1)(m+1) + j
+			if (i >= 2) & (j >= 2) & (i <= n) & (j <= m)
+				A((i-1)*(m+1) + j, i*(m+1) + j) = 1;
+				A((i-1)*(m+1) + j, (i-1)*(m+1) + j-1) = -2*lamda;
+				A((i-1)*(m+1) + j, (i-1)*(m+1) + j) = 4*lamda;
+				A((i-1)*(m+1) + j, (i-1)*(m+1) + j+1) = -2*lamda;
+				A((i-1)*(m+1) + j, (i-2)*(m+1) + j) = -1;
+
+				b((i-1)*(m+1) + j) = 0;
+			elseif i == 1
+				A((i-1)*(m+1) + j, (i-1)*(m+1) + j) = 1;
+				b((i-1)*(m+1) + j) = f((j-1)*k);
+				
+
+		A(1:m+2:end) = 1 + 2*lamda;
+		A(2:m+2:end) = -lamda;
+		A(m+2:m+2:end) = -lamda;
+
+		A(1,1) = 1;
+		A(1,2) = 0;
+		A(m+1,m+1) = 1;
+		A(m+1,m) = 0;
+
+		b(2:m) = U(i-1,2:m) + k*fun((1:m-1)*h,(i-1)*k);
+		b(1) = U(i,1);
+		b(end) = U(i,end);
+
+		U(i,:) = (A\b)';
+	end
+
+	U
+end
---
+++ abheekMA473lab1/q4.m
@@ -0,0 +1,64 @@
+function q1
+	close all; clear;
+	h = 0.25;
+	k = 0.05;
+	m = 0.5/h;
+	n = 50;
+
+	X = (0:n)*k;
+	Y = (0:m)*h;
+	U = Crank(h, k, m, n, @fun, @f, @g1, @g2);
+	% size(X), size(Y), size(U)
+	figure; plot(U(end, :));
+	% saveas(gcf, 'plots/q1_1.png');
+	figure; surf(X, Y, U');
+	% saveas(gcf, 'plots/q1_2.png');
+end
+
+function [y] = fun(x, t)
+	y = 0;
+end
+
+function [y] = f(x)
+	y = x.*(1-x);
+end
+
+function [y] = g1(t)
+	y = 0;
+end
+
+function [y] = g2(t)
+	y = t.^2;
+end
+
+function [U] = Crank(h, k, m, n, fun, f, g1, g2)
+	fprintf('\nRunning Crank Nicolson\n');
+	lamda = k / h^2;
+	U = zeros(n+1, m+1);
+
+	U(1, 1:end) = f((0:m)*h);
+	U(1:end, 1) = g1((0:n)*k);
+	U(1:end, end) = g2((0:n)*k);
+
+	for i = 2:n+1
+		A = zeros(m+1, m+1);
+		b = zeros(m+1, 1);
+
+		A(1:m+2:end) = 1 + lamda;
+		A(2:m+2:end) = -lamda/2;
+		A(m+2:m+2:end) = -lamda/2;
+
+		A(1,1) = 1;
+		A(1,2) = 0;
+		A(m+1,m+1) = 1/h;
+		A(m+1,m) = -1/h;
+
+		b(2:m) = U(i-1,1:m-1)*lamda/2 + (1-lamda)*U(i-1,2:m) + U(i-1,3:m+1)*lamda/2 + k*fun((1:m-1)*h,(i-1)*k);
+		b(1) = U(i,1);
+		b(end) = U(i,end);
+
+		U(i,:) = (A\b)';
+	end
+
+	U
+end
